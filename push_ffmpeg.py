38de5cff84813c42eddabea6736be21ec61e5179
Aanis Noor <38424838+oron-sinaa@users.noreply.github.com>
2022-11-30 16:52:58 +0530

    Update push_ffmpeg.py

--- push_ffmpeg.py
+++ push_ffmpeg.py
@@ -1,24 +1,37 @@
-import subprocess
+import os
 import csv
+import subprocess
 import multiprocessing
 
-HOST_IP = "10.150.101.18"
-TRANSPORT = 'tcp' #udp
+HOST_IP = os.environ.get("HOST_IP")
+RTSP_PORT = os.environ.get("RTSP_PORT")
+TRANSPORT = "tcp" #udp
+CSV_PATH = "RTSP_FEEDS.csv"
 
 def ensureFfmpeg(cmd):
-    print("Here with a subprocess!" + cmd[-1])
+    """
+        * Makes sure stream restart in case of push failure.
+        * Multiple instances can run.
+    """
+    print("[info] ffmpeg push subprocess [feed] " + cmd[-1])
     retval = 1
+    # Restarts if process has not exited normally (with a passed terminate signal)
     while retval != 0:
-        retval = subprocess.call(cmd)
+        retval = subprocess.call(cmd, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
 
 def main():
-    with open("/home/aanisnoor/Personal/Work/Misc/adani/RTSP_FEEDS.csv") as filereader:
+    """
+        csv format (each line should with \\n):
+        * rtsp://<user>:<pass>@<ip>:<port>,<streamname>.stream
+    """
+    with open(CSV_PATH) as filereader:
         reader = csv.reader(filereader)
         numLines = 0
         cmds = []
+        # For every stream in csv
         for line in reader:
-            source, destiny = line[0], f"rtsp://{HOST_IP}:1935/live/{line[1]}"
-            cmd = ['ffmpeg', '-re', '-rtsp_transport', f'{TRANSPORT}', '-i', f'{source}', '-c', 'copy', '-rtsp_transport', 'tcp', '-f', 'rtsp', f'{destiny}']
+            source, destiny = line[0], f"rtsp://{HOST_IP}:{RTSP_PORT}/live/{line[1]}"
+            cmd = ['ffmpeg', '-re', '-rtsp_transport', f'{TRANSPORT}', '-i', f'{source}', '-c', 'copy', '-b:v', '2M', '-rtsp_transport', 'tcp', '-f', 'rtsp', f'{destiny}']
             cmds.append(cmd)
             numLines += 1
         pool = multiprocessing.Pool(processes=numLines)
